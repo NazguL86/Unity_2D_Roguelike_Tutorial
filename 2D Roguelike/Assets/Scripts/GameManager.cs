9abd8e23fbfcab11b5a3b44180be602fb2574d99
Alberto Cerati <nazgul86@gmail.com>
2018-11-11 16:56:17 -0800

    Adding Unity Mechanics

--- 2D Roguelike/Assets/Scripts/GameManager.cs
+++ 2D Roguelike/Assets/Scripts/GameManager.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     public static GameManager instance = null;
-
+    public float turnDelay = 0.1f;
     public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
 
     private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
 
 	void Awake () {
         if (instance == null) {
@@ -18,12 +22,44 @@ public class GameManager : MonoBehaviour {
         }
 
         DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
         InitGame();
 	}
 	
 	// Update is called once per frame
 	void InitGame () {
+        enemies.Clear();
         boardScript.SetupScene(level);
 	}
+
+    public void GameOver() {
+        enabled = false;
+    }
+
+    void Update() {
+        if (playersTurn || enemiesMoving) {
+            return;
+        }
+
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script) {
+        enemies.Add(script);
+    }
+
+    private IEnumerator MoveEnemies() {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0) {
+            yield return new WaitForSeconds(turnDelay);
+        }
+        for (int i = 0; i < enemies.Count; i++) {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+        playersTurn = true;
+        enemiesMoving = false;
+    }
 }
