897f1d9bbe58351f0c6f4dd2c1b733cc7563ccc8
Alberto Cerati <ceratia@amazon.com>
2018-11-13 22:13:15 -0800

    Adding UI

--- 2D Roguelike/Assets/Scripts/Player.cs
+++ 2D Roguelike/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MovingObject {
 
@@ -9,6 +10,7 @@ public class Player : MovingObject {
     public int pointsPerFood = 10;
     public int pointsPerSoda = 20;
     public float restartLevelDelay = 1f;
+    public Text foodText;
 
     private Animator animator;
     private int food;
@@ -18,6 +20,9 @@ public class Player : MovingObject {
         animator = GetComponent<Animator>();
 
         food = GameManager.instance.playerFoodPoints;
+
+        foodText.text = "Food: " + food;
+
         base.Start();
 	}
 
@@ -50,9 +55,11 @@ public class Player : MovingObject {
             enabled = false;
         } else if (other.tag == "Food") {
             food += pointsPerFood;
+            foodText.text = "+ " + pointsPerFood + " Food: "+food;
             other.gameObject.SetActive(false);
         } else if (other.tag == "Soda") {
             food += pointsPerSoda;
+            foodText.text = "+ " + pointsPerSoda + " Food: " + food;
             other.gameObject.SetActive(false);
         }
     }
@@ -70,12 +77,14 @@ public class Player : MovingObject {
     public void LooseFood(int loss) {
         animator.SetTrigger("PlayerHit");
         food -= loss;
+        foodText.text = "- " + loss + " Food: " + food;
         CheckIfGameOver();
     }
 
     protected override void AttemptMove<T>(int xDir, int yDir)
     {
         food--;
+        foodText.text = "Food: " + food;
         base.AttemptMove<T>(xDir, yDir);
         RaycastHit2D hit;
         CheckIfGameOver();
